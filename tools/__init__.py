9d54c3fdc64730228d35677044b54769b606019f
smtdfc <smtdfc.master@gmail.com>
2025-06-21 23:48:06 +0700

    fix: Fix typing problems

--- tools/__init__.py
+++ tools/__init__.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from .schema import *
 
 class ProjectQuery(BaseTool):
-  name="anos_project_query"
-  description="Access operations with the project including, getting file information, creating files, deleting files"
+  name:str="anos_project_query"
+  description:str="Access operations with the project including, getting file information, creating files, deleting files"
   args:Type[BaseModel] = ProjectQueryArgSchema
 
