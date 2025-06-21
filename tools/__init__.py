b47ff09ae8fb7c15b83afa4760dde0fb976a50da
smtdfc <smtdfc.master@gmail.com>
2025-06-21 23:58:27 +0700

    feat: Add project query actions

--- tools/__init__.py
+++ tools/__init__.py
@@ -5,7 +5,14 @@ from .schema import *
 
 class ProjectQuery(BaseTool):
     name: str = "project_query"
-    description: str = "Query project"
+    description: str = "Query project: create files, remove files, move files, get project info"
 
-    def _run(self, query: Union[ProjectQueryAddFileSchema,ProjectQueryRemoveFileSchema]) -> str:
+    def _run(
+        self, 
+        query: Union[
+            ProjectQueryAddFileSchema,
+            ProjectQueryRemoveFileSchema,
+            ProjectQueryInfoSchema,
+            ProjectQueryMoveFileSchema
+    ]) -> str:
         return f""
--- tools/schema.py
+++ tools/schema.py
@@ -10,5 +10,19 @@ class ProjectQueryRemoveFileSchema(BaseModel):
   type: str = 'remove_file'
   path:str 
 
+class ProjectQueryMoveFileSchema(BaseModel):
+  type: str = 'move_file'
+  source:str 
+  target:str 
+
+class ProjectQueryInfoSchema(BaseModel):
+  type: str = 'project_info'
+
+
 class ProjectQueryArgSchema(BaseModel):
-  query: Union[ProjectQueryAddFileSchema,ProjectQueryRemoveFileSchema] = Field(..., description='Query')
+  query: Union[
+    ProjectQueryAddFileSchema,
+    ProjectQueryRemoveFileSchema,
+    ProjectQueryInfoSchema,
+    ProjectQueryMoveFileSchema
+  ] = Field(..., description='Query')
