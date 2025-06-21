a9af12575c8f76baac1cc5ad92b545436186cf36
smtdfc <smtdfc.master@gmail.com>
2025-06-21 23:52:12 +0700

    fix: Fix tool setup error

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,10 +1,11 @@
 from langchain_core.tools import BaseTool
-from typing import Optional, Type
+from typing import Optional, Type,Union
 from pydantic import BaseModel
 from .schema import *
 
 class ProjectQuery(BaseTool):
-  name:str="anos_project_query"
-  description:str="Access operations with the project including, getting file information, creating files, deleting files"
-  args:Type[BaseModel] = ProjectQueryArgSchema
+    name: str = "project_query"
+    description: str = "Query project"
 
+    def _run(self, query: Union[ProjectQueryAddFileSchema,ProjectQueryRemoveFileSchema]) -> str:
+        return f""
