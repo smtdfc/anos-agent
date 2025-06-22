a017a3a17438b06e3fbab8454bef4aec0fe5a521
smtdfc <smtdfc.master@gmail.com>
2025-06-22 12:50:47 +0700

    fix: Fix tool arguments format problems

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,29 +1,21 @@
 from langchain_core.tools import BaseTool
-from work import  ProjectManagement
-from typing import Optional, Type,Union
-from pydantic import BaseModel
+from work import ProjectManagement
+from typing import Union
+from pydantic import BaseModel, Field
 from .schema import *
-import logging
 
 
 class ProjectQuery(BaseTool):
     name: str = "project_query"
     description: str = "Query project: create project, create files, remove files, move files, get project info"
+    args_schema: Type[BaseModel] = ProjectQueryArgs  
 
-    def _run(
-        self, 
-        query: Union[
-            ProjectQueryAddFileSchema,
-            ProjectQueryRemoveFileSchema,
-            ProjectQueryInfoSchema,
-            ProjectQueryMoveFileSchema,
-            ProjectQueryCreateSchema
-        ]
-    ) -> any:
+    def _run(self, query: BaseModel) -> any:
         try:
-            if query.type == 'create_project':
-                logging.info(f'Creating project:{query.project_name}')
-                return ProjectManagement.create(query.project_name)
-            return f""
+            query_obj = query.query 
+            if query_obj.type == 'create_project':
+                logging.info(f'Creating project: {query_obj.project_name}')
+                return ProjectManagement.create(query_obj.project_name)
+            return f"Query type '{query_obj.type}' unsupported !"
         except Exception as e:
             return f"Error: Unable to complete action: {str(e)}"
