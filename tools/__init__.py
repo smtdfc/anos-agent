bece9e17611f49691ec24d18bbd404587c0eea76
smtdfc <smtdfc.master@gmail.com>
2025-06-22 12:55:22 +0700

    fix: Fix tool description

--- tools/__init__.py
+++ tools/__init__.py
@@ -7,7 +7,19 @@ from .schema import *
 
 class ProjectQuery(BaseTool):
     name: str = "project_query"
-    description: str = "Query project: create project, create files, remove files, move files, get project info"
+    description = (
+        "Use this tool to manage a project. The `query` field must be an object with a `type` key.\n"
+        "Available query types:\n"
+        "- 'create_project': create a project, requires 'project_name'\n"
+        "- 'add_file': add a file, requires 'file_name' and 'path'\n"
+        "- 'remove_file': remove a file, requires 'path'\n"
+        "- 'move_file': move a file, requires 'source' and 'target'\n"
+        "- 'project_info': get project info, no additional fields\n\n"
+        "**Examples**:\n"
+        "- To create a project: `{'query': {'type': 'create_project', 'project_name': 'aurora'}}`\n"
+        "- To add a file: `{'query': {'type': 'add_file', 'file_name': 'main.py', 'path': '/src'}}`\n"
+    )
+    
     args_schema: Type[BaseModel] = ProjectQueryArgSchema  
 
     def _run(self, query: BaseModel) -> any:
