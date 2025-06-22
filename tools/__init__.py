fe0adfbebf8f5608d6f69b503c80dbf544e2f797
smtdfc <smtdfc.master@gmail.com>
2025-06-22 13:11:37 +0700

    update

--- tools/__init__.py
+++ tools/__init__.py
@@ -5,29 +5,12 @@ from pydantic import BaseModel, Field
 from .schema import *
 
 
-class ProjectQuery(BaseTool):
-    name: str = "project_query"
-    description:str = (
-        "Use this tool to manage a project. The `query` field must be an object with a `type` key.\n"
-        "Available query types:\n"
-        "- 'create_project': create a project, requires 'project_name'\n"
-        "- 'add_file': add a file, requires 'file_name' and 'path'\n"
-        "- 'remove_file': remove a file, requires 'path'\n"
-        "- 'move_file': move a file, requires 'source' and 'target'\n"
-        "- 'project_info': get project info, no additional fields\n\n"
-        "**Examples**:\n"
-        "- To create a project: `{'query': {'type': 'create_project', 'project_name': 'aurora'}}`\n"
-        "- To add a file: `{'query': {'type': 'add_file', 'file_name': 'main.py', 'path': '/src'}}`\n"
-    )
+class CreateProjectTool(BaseTool):
+    name: str = "create_project"
+    description:str = 'Create a new project'
     
-    args_schema: Type[BaseModel] = ProjectQueryArgSchema  
+    args_schema: Type[BaseModel] = CreateProjectSchema  
 
-    def _run(self, query: BaseModel) -> any:
-        try:
-            query_obj = query.query 
-            if query_obj.type == 'create_project':
-                logging.info(f'Creating project: {query_obj.project_name}')
-                return ProjectManagement.create(query_obj.project_name)
-            return f"Query type '{query_obj.type}' unsupported !"
-        except Exception as e:
-            return f"Error: Unable to complete action: {str(e)}"
+    def _run(self, name:str) -> any:
+        logging.info(f'Creating project: {name}')
+        return ProjectManagement.create(name)
--- tools/schema.py
+++ tools/schema.py
@@ -1,32 +1,5 @@
 from pydantic import BaseModel, Field
 from typing import Union, Literal
 
-class ProjectQueryAddFileSchema(BaseModel):
-    type: Literal['add_file']
-    file_name: str
-    path: str
-
-class ProjectQueryRemoveFileSchema(BaseModel):
-    type: Literal['remove_file']
-    path: str
-
-class ProjectQueryMoveFileSchema(BaseModel):
-    type: Literal['move_file']
-    source: str
-    target: str
-
-class ProjectQueryInfoSchema(BaseModel):
-    type: Literal['project_info']
-
-class ProjectQueryCreateSchema(BaseModel):
-    type: Literal['create_project']
-    project_name: str
-
-class ProjectQueryArgSchema(BaseModel):
-    query: Union[
-        ProjectQueryAddFileSchema,
-        ProjectQueryRemoveFileSchema,
-        ProjectQueryInfoSchema,
-        ProjectQueryMoveFileSchema,
-        ProjectQueryCreateSchema
-    ] = Field(..., discriminator='type')
+class CreateProjectSchema(BaseModel):
+    name:str = Field(..., description='Project name')
